f3f4941f160a01d40c1b83c45e6ab061c432b9e5
Vitor Capdeville <vgcapdeville@hotmail.com>
2025-01-26 19:09:11 -0300

    ♻️ Configurar cache e revalidate.

--- app/clientConfig.ts
+++ app/clientConfig.ts
@@ -1,9 +1,11 @@
-import { client } from "@/app/client/sdk.gen";
+import { client } from "@/app/client/sdk.gen"; // !! PRECISA RODAR ANTES DO getToken !!
 import { getToken } from "@/app/actions/login";
 
 client.setConfig({
   baseUrl: process.env.BACKEND_URL,
   auth: () => getToken(),
+  cache: "force-cache",
+  next: { revalidate: 3600 },
 });
 
 export { client };
